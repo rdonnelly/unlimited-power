a7e2397be028601d737a9153df7a2a739103cddb
Ryan Donnelly <ryanjdonnelly@gmail.com>
2024-02-13 13:31:09 -0500

    add version info to info sheet

--- src/screens/InfoScreen.tsx
+++ src/screens/InfoScreen.tsx
@@ -1,5 +1,6 @@
 import { A } from '@expo/html-elements';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import * as Application from 'expo-application';
+import { StyleSheet, Text, View } from 'react-native';
 
 import { useTheme } from '@hooks/useTheme';
 import type { InfoScreenProps } from '@navigation/types';
@@ -8,34 +9,36 @@ export function InfoScreen({ navigation }: InfoScreenProps) {
   const { themeStyles } = useTheme();
 
   return (
-    <ScrollView style={styles.scrollContainer}>
-      <View style={styles.container}>
-        <View style={styles.disclaimer}>
-          <Text style={[styles.disclaimerText, themeStyles.themedColor]}>
-            The information presented in this app about Star Wars Unlimited,
-            both literal and graphical, is copyrighted by Fantasy Flight Games.
-            This app is not produced by, endorsed by, supported by, or
-            affiliated with Fantasy Flight Games.
-          </Text>
-        </View>
+    <View style={styles.container}>
+      <View style={styles.disclaimer}>
+        <Text style={[styles.disclaimerText, themeStyles.themedColor]}>
+          The information presented in this app about Star Wars Unlimited, both
+          literal and graphical, is copyrighted by Fantasy Flight Games. This
+          app is not produced by, endorsed by, supported by, or affiliated with
+          Fantasy Flight Games.
+        </Text>
+      </View>
 
-        <A href="https://rdonnelly.com" style={styles.credit}>
-          <>
-            <Text style={[styles.creditText, themeStyles.themedColor]}>{`
+      <View style={styles.version}>
+        <Text style={[styles.versionText, themeStyles.themedColor]}>
+          {Application.applicationName} {Application.nativeApplicationVersion} (
+          {Application.nativeBuildVersion})
+        </Text>
+      </View>
+
+      <A href="https://rdonnelly.com" style={styles.credit}>
+        <>
+          <Text style={[styles.creditText, themeStyles.themedColor]}>{`
 Designed and Developed by
 Ryan Donnelly
             `}</Text>
-          </>
-        </A>
-      </View>
-    </ScrollView>
+        </>
+      </A>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  scrollContainer: {
-    flex: 1,
-  },
   container: {
     alignItems: 'center',
     flex: 1,
@@ -43,10 +46,19 @@ const styles = StyleSheet.create({
     paddingTop: 16,
   },
   disclaimer: {
+    flex: 1,
     padding: 16,
     paddingBottom: 32,
   },
   disclaimerText: {
+    textAlign: 'justify',
+  },
+  version: {
+    padding: 16,
+    paddingBottom: 32,
+  },
+  versionText: {
+    fontWeight: '600',
     textAlign: 'justify',
   },
   credit: {
