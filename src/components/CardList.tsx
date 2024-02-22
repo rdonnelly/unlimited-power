e0e33df459d63cdf1df0ec3f5dfddcba5c4a4ab8
Ryan Donnelly <ryanjdonnelly@gmail.com>
2024-02-21 20:27:23 -0500

    refactor card list screen and card list

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,17 +1,14 @@
 import { FlashList } from '@shopify/flash-list';
+import { useCallback, useMemo } from 'react';
 import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
 
+import { Button } from '@components/Button';
 import { CardListItem, ITEM_HEIGHT } from '@components/CardListItem';
-import { type CardsResponseData } from '@data/CardsResponse';
+import { useCards } from '@data/hooks/useCards';
 import { useTheme } from '@hooks/useTheme';
 import { DARK_THEME, LIGHT_THEME } from '@styles/theme';
 
-export type CardListProps = {
-  cards: CardsResponseData;
-  cardCount?: number;
-  showLoading: boolean;
-  hasNextPage: boolean;
-  fetchNextPage: () => void;
+type CardListProps = {
   onPressItem: (
     id: number,
     index: number,
@@ -20,54 +17,122 @@ export type CardListProps = {
   ) => void;
 };
 
-export const CardList = ({
-  cards,
-  cardCount,
-  showLoading,
-  hasNextPage,
-  fetchNextPage,
-  onPressItem: handlePressItem,
-}: CardListProps) => {
+export function CardList({ onPressItem: handlePressItem }: CardListProps) {
   const { theme, themeStyles } = useTheme();
 
-  const loadNextPage = () => {
+  const {
+    data,
+    isFetching,
+    isLoading,
+    isError,
+    hasNextPage,
+    fetchNextPage,
+    refetch,
+  } = useCards();
+
+  const cards = useMemo(() => {
+    return data?.pages.flatMap((page) => page.data) ?? [];
+  }, [data]);
+
+  const cardCount = useMemo(() => {
+    return data?.pages[0]?.meta.pagination.total;
+  }, [data]);
+
+  const loadNextPage = useCallback(() => {
     if (hasNextPage) {
       fetchNextPage();
     }
-  };
+  }, [hasNextPage, fetchNextPage]);
+
+  if (cards && cards.length) {
+    return (
+      <View style={styles.container}>
+        <View style={[styles.listContainer, themeStyles.themedbackground0]}>
+          <FlashList
+            data={cards}
+            renderItem={({ item: card, index }) => (
+              <CardListItem
+                card={card}
+                index={index}
+                onPress={handlePressItem}
+              />
+            )}
+            estimatedItemSize={ITEM_HEIGHT}
+            onEndReached={loadNextPage}
+            onEndReachedThreshold={1.5}
+            ListFooterComponent={
+              <View
+                style={[styles.listFooter, themeStyles.themedbackground100]}
+              >
+                {isLoading || isFetching || cardCount == null ? (
+                  <ActivityIndicator
+                    color={
+                      theme.scheme === 'light'
+                        ? LIGHT_THEME.tintSubdued
+                        : DARK_THEME.tintSubdued
+                    }
+                  />
+                ) : (
+                  <Text style={styles.listFooterText}>
+                    {cardCount} Cards Found
+                  </Text>
+                )}
+              </View>
+            }
+          />
+        </View>
+      </View>
+    );
+  }
+
+  if (isError) {
+    return (
+      <View style={styles.container}>
+        <View style={styles.error}>
+          <Text style={[styles.errorText, themeStyles.themedColorSubdued]}>
+            An unknown error occured while fetching card data.
+          </Text>
+        </View>
+
+        <Button variant="bold" onPress={() => refetch()}>
+          Retry
+        </Button>
+      </View>
+    );
+  }
 
   return (
-    <View style={[styles.container, themeStyles.themedbackground0]}>
-      <FlashList
-        data={cards}
-        renderItem={({ item: card, index }) => (
-          <CardListItem card={card} index={index} onPress={handlePressItem} />
-        )}
-        estimatedItemSize={ITEM_HEIGHT}
-        onEndReached={loadNextPage}
-        onEndReachedThreshold={1.5}
-        ListFooterComponent={
-          <View style={[styles.listFooter, themeStyles.themedbackground100]}>
-            {showLoading || cardCount == null ? (
-              <ActivityIndicator
-                color={
-                  theme.scheme === 'light'
-                    ? LIGHT_THEME.tintSubdued
-                    : DARK_THEME.tintSubdued
-                }
-              />
-            ) : (
-              <Text style={styles.listFooterText}>{cardCount} Cards Found</Text>
-            )}
-          </View>
-        }
-      />
+    <View style={styles.container}>
+      <View style={styles.activity}>
+        <ActivityIndicator
+          color={
+            theme.scheme === 'light'
+              ? LIGHT_THEME.tintSubdued
+              : DARK_THEME.tintSubdued
+          }
+        />
+      </View>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+    width: '100%',
+  },
+  activity: {},
+  error: {
+    marginBottom: 32,
+    maxWidth: 240,
+  },
+  errorText: {
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  listContainer: {
     flex: 1,
     width: '100%',
   },
--- src/screens/CardListScreen.tsx
+++ src/screens/CardListScreen.tsx
@@ -1,23 +1,11 @@
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { useCallback, useEffect, useMemo } from 'react';
-import {
-  ActivityIndicator,
-  Pressable,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import { useCallback, useEffect } from 'react';
+import { Pressable, StyleSheet, View } from 'react-native';
 
-import { Button } from '@components/Button';
 import { CardList } from '@components/CardList';
-import { useCards } from '@data/hooks/useCards';
-import { useTheme } from '@hooks/useTheme';
 import type { CardListScreenProps } from '@navigation/types';
-import { DARK_THEME, LIGHT_THEME } from '@styles/theme';
 
 export function CardListScreen({ navigation }: CardListScreenProps) {
-  const { theme, themeStyles } = useTheme();
-
   useEffect(() => {
     navigation.setOptions({
       headerRight: ({ tintColor }) => {
@@ -41,25 +29,7 @@ export function CardListScreen({ navigation }: CardListScreenProps) {
     });
   }, [navigation]);
 
-  const {
-    data,
-    isFetching,
-    isLoading,
-    isError,
-    hasNextPage,
-    fetchNextPage,
-    refetch,
-  } = useCards();
-
-  const cards = useMemo(() => {
-    return data?.pages.flatMap((page) => page.data) ?? [];
-  }, [data]);
-
-  const cardCount = useMemo(() => {
-    return data?.pages[0]?.meta.pagination.total;
-  }, [data]);
-
-  const handlePressItem = useCallback(
+  const navigateToCardDetails = useCallback(
     (id: number, index: number, title: string, caption?: string) => {
       if (navigation) {
         navigation.push('StackCardDetailScreen', {
@@ -73,50 +43,9 @@ export function CardListScreen({ navigation }: CardListScreenProps) {
     [navigation],
   );
 
-  if (cards && cards.length) {
-    return (
-      <View style={styles.container}>
-        <>
-          <CardList
-            cards={cards}
-            cardCount={cardCount}
-            showLoading={isFetching || isLoading}
-            hasNextPage={!!hasNextPage}
-            fetchNextPage={fetchNextPage}
-            onPressItem={handlePressItem}
-          />
-        </>
-      </View>
-    );
-  }
-
-  if (isError) {
-    return (
-      <View style={styles.container}>
-        <View style={styles.error}>
-          <Text style={[styles.errorText, themeStyles.themedColorSubdued]}>
-            An unknown error occured while fetching card data.
-          </Text>
-        </View>
-
-        <Button variant="bold" onPress={() => refetch()}>
-          Retry
-        </Button>
-      </View>
-    );
-  }
-
   return (
     <View style={styles.container}>
-      <View style={styles.activity}>
-        <ActivityIndicator
-          color={
-            theme.scheme === 'light'
-              ? LIGHT_THEME.tintSubdued
-              : DARK_THEME.tintSubdued
-          }
-        />
-      </View>
+      <CardList onPressItem={navigateToCardDetails} />
     </View>
   );
 }
