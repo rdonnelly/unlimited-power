e1a5375af3762e39ce7cc9535dae5a08955659ff
Ryan Donnelly <ryanjdonnelly@gmail.com>
2024-02-25 14:38:04 -0500

    adjust bottom sheet compatibility

--- src/components/CardListBottomSheet.tsx
+++ src/components/CardListBottomSheet.tsx
@@ -1,4 +1,7 @@
-import BottomSheet, { BottomSheetTextInput } from '@gorhom/bottom-sheet';
+import BottomSheet, {
+  BottomSheetScrollView,
+  BottomSheetTextInput,
+} from '@gorhom/bottom-sheet';
 import type { BottomSheetMethods } from '@gorhom/bottom-sheet/lib/typescript/types';
 import debounce from 'lodash/debounce';
 import { useCallback, useMemo, useRef } from 'react';
@@ -55,6 +58,10 @@ export function CardListBottomSheet({
     [setSearchStringDebounced],
   );
 
+  const handleSubmitEditing = useCallback(() => {
+    bottomSheetRef.current?.collapse();
+  }, []);
+
   const [aspectOptions, addAspect, removeAspect] = useAspectFilterStore(
     (state) => [state.aspects, state.add, state.remove],
   );
@@ -73,30 +80,37 @@ export function CardListBottomSheet({
     <BottomSheet
       ref={bottomSheetRef}
       snapPoints={snapPoints}
-      // index={0}
+      index={0}
       backgroundStyle={[styles.background, themeStyles.themedbackground100]}
       handleIndicatorStyle={{ backgroundColor: theme.textSubdued }}
       keyboardBehavior="fillParent"
-      keyboardBlurBehavior="restore"
+      keyboardBlurBehavior="none"
     >
       <View style={styles.container}>
-        <BottomSheetTextInput
-          style={[
-            styles.input,
-            themeStyles.themedbackground200,
-            themeStyles.themedColor,
-          ]}
-          autoCapitalize="none"
-          autoCorrect={false}
-          clearButtonMode="always"
-          placeholder="Search"
-          placeholderTextColor={theme.textSubdued}
-          returnKeyType="search"
-          onChangeText={handleChangeText}
-          defaultValue={searchString ?? ''}
-        />
+        <View style={styles.inputContainer}>
+          <BottomSheetTextInput
+            style={[
+              styles.input,
+              themeStyles.themedbackground200,
+              themeStyles.themedColor,
+            ]}
+            autoCapitalize="none"
+            autoCorrect={false}
+            clearButtonMode="always"
+            placeholder="Search"
+            placeholderTextColor={theme.textSubdued}
+            returnKeyType="search"
+            onChangeText={handleChangeText}
+            onSubmitEditing={handleSubmitEditing}
+            defaultValue={searchString ?? ''}
+          />
+        </View>
 
-        {/* <View style={styles.filterSection}>
+        <BottomSheetScrollView
+          style={styles.scrollContainer}
+          contentContainerStyle={styles.scrollContainerContent}
+        >
+          {/* <View style={styles.filterSection}>
           <View style={styles.filterSectionHeader}>
             <Text style={styles.filterSectionHeaderText}>Aspects</Text>
           </View>
@@ -116,53 +130,60 @@ export function CardListBottomSheet({
           />
         </View> */}
 
-        <View style={styles.filterSection}>
-          <View style={styles.filterSectionHeader}>
-            <Text
-              style={[styles.filterSectionHeaderText, themeStyles.themedColor]}
-            >
-              Type
-            </Text>
+          <View style={styles.filterSection}>
+            <View style={styles.filterSectionHeader}>
+              <Text
+                style={[
+                  styles.filterSectionHeaderText,
+                  themeStyles.themedColor,
+                ]}
+              >
+                Type
+              </Text>
+            </View>
+            <Chips
+              options={typeFilterOptions.map((type) => ({
+                key: type,
+                label: type,
+              }))}
+              selectedOptions={typeOptions}
+              onChange={(type, isSelected) => {
+                if (isSelected) {
+                  removeType(type);
+                } else {
+                  addType(type);
+                }
+              }}
+            />
           </View>
-          <Chips
-            options={typeFilterOptions.map((type) => ({
-              key: type,
-              label: type,
-            }))}
-            selectedOptions={typeOptions}
-            onChange={(type, isSelected) => {
-              if (isSelected) {
-                removeType(type);
-              } else {
-                addType(type);
-              }
-            }}
-          />
-        </View>
 
-        <View style={styles.filterSection}>
-          <View style={styles.filterSectionHeader}>
-            <Text
-              style={[styles.filterSectionHeaderText, themeStyles.themedColor]}
-            >
-              Rarity
-            </Text>
+          <View style={styles.filterSection}>
+            <View style={styles.filterSectionHeader}>
+              <Text
+                style={[
+                  styles.filterSectionHeaderText,
+                  themeStyles.themedColor,
+                ]}
+              >
+                Rarity
+              </Text>
+            </View>
+            <Chips
+              options={rarityFilterOptions.map((rarity) => ({
+                key: rarity,
+                label: rarity,
+              }))}
+              selectedOptions={rarityOptions}
+              onChange={(rarity, isSelected) => {
+                if (isSelected) {
+                  removeRarity(rarity);
+                } else {
+                  addRarity(rarity);
+                }
+              }}
+            />
           </View>
-          <Chips
-            options={rarityFilterOptions.map((rarity) => ({
-              key: rarity,
-              label: rarity,
-            }))}
-            selectedOptions={rarityOptions}
-            onChange={(rarity, isSelected) => {
-              if (isSelected) {
-                removeRarity(rarity);
-              } else {
-                addRarity(rarity);
-              }
-            }}
-          />
-        </View>
+        </BottomSheetScrollView>
       </View>
     </BottomSheet>
   );
@@ -172,20 +193,29 @@ const styles = StyleSheet.create({
   background: {},
   container: {
     flex: 1,
-    alignItems: 'center',
+  },
+  inputContainer: {
+    marginBottom: 16,
     paddingHorizontal: 16,
+    width: '100%',
   },
   input: {
     borderRadius: 8,
     borderWidth: 0,
     fontSize: 16,
+    height: 44,
     lineHeight: 20,
-    marginBottom: 40,
     padding: 12,
     width: '100%',
   },
+  scrollContainer: {
+    width: '100%',
+  },
+  scrollContainerContent: {
+    paddingHorizontal: 16,
+    paddingTop: 24,
+  },
   filterSection: {
-    alignItems: 'flex-start',
     marginBottom: 16,
     width: '100%',
   },
@@ -193,7 +223,6 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   filterSectionHeaderText: {
-    textAlign: 'center',
     fontSize: 20,
     fontWeight: '700',
   },
