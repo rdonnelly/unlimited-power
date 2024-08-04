c3683297bd60952a61e609238c1dbf76d5bf5866
Ryan Donnelly <ryanjdonnelly@gmail.com>
2024-08-04 14:54:51 -0400

    add TWI and C24 expansions

--- src/data/Expansion.ts
+++ src/data/Expansion.ts
@@ -3,6 +3,8 @@ import * as z from 'zod';
 const VALUES = [
   'SOR', // 2, Spark of Rebellion
   'SHD', // 8, Shadows of the Galaxy
+  'TWI', // 18, Twilight of the Republic
+  'C24', // 13, 2024 Convention Exclusive
 ] as const;
 export const ExpansionEnum = z.enum(VALUES);
 export type Expansion = z.infer<typeof ExpansionEnum>;
@@ -10,14 +12,20 @@ export type Expansion = z.infer<typeof ExpansionEnum>;
 export const ExpansionRank: Record<Expansion, number> = {
   SOR: 1,
   SHD: 2,
+  TWI: 3,
+  C24: 4,
 } as const;
 
 export const ExpansionCodes: Record<Expansion, number> = {
   SOR: 2,
   SHD: 8,
+  TWI: 18,
+  C24: 13,
 } as const;
 
 export const ExpansionNames: Record<Expansion, string> = {
   SOR: 'Spark of Rebellion',
   SHD: 'Shadows of the Galaxy',
+  TWI: 'Twilight of the Republic',
+  C24: '2024 Convention Exclusive',
 } as const;
