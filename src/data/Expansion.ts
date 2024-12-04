9021145ba8b45c02b12c2bf4fbefe2b81c05a483
Ryan Donnelly <ryanjdonnelly@gmail.com>
2024-12-04 11:23:44 -0500

    add JTL to expansions

--- src/data/Expansion.ts
+++ src/data/Expansion.ts
@@ -5,6 +5,7 @@ const VALUES = [
   'SHD', // 8, Shadows of the Galaxy
   'TWI', // 18, Twilight of the Republic
   'C24', // 13, 2024 Convention Exclusive
+  'JTL', // 23, Jump to Lightspeed
 ] as const;
 export const ExpansionEnum = z.enum(VALUES);
 export type Expansion = z.infer<typeof ExpansionEnum>;
@@ -13,7 +14,8 @@ export const ExpansionRank: Record<Expansion, number> = {
   SOR: 1,
   SHD: 2,
   TWI: 3,
-  C24: 4,
+  JTL: 4,
+  C24: 5,
 } as const;
 
 export const ExpansionCodes: Record<Expansion, number> = {
@@ -21,11 +23,13 @@ export const ExpansionCodes: Record<Expansion, number> = {
   SHD: 8,
   TWI: 18,
   C24: 13,
+  JTL: 23,
 } as const;
 
 export const ExpansionNames: Record<Expansion, string> = {
   SOR: 'Spark of Rebellion',
   SHD: 'Shadows of the Galaxy',
   TWI: 'Twilight of the Republic',
+  JTL: 'Jump to Lightspeed',
   C24: '2024 Convention Exclusive',
 } as const;
