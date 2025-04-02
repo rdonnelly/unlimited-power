0b35d2275e1f5ae1f9d34b13b36f37cb6d034de3
Ryan Donnelly <ryanjdonnelly@gmail.com>
2025-04-02 13:00:22 -0400

    update url error message to try again

--- src/utils/OpenURL.ts
+++ src/utils/OpenURL.ts
@@ -4,7 +4,7 @@ export const openURL = async (url: string) => {
   try {
     await Linking.openURL(url);
   } catch {
-    Alert.alert('Karabast!', 'The URL could not be opened, sorry!', [
+    Alert.alert('Karabast!', 'The URL could not be opened, please try again!', [
       { text: 'OK' },
     ]);
   }
