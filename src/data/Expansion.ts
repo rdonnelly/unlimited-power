2fd00f7f335108478d47d7ace6cd71cdf5e33990
Ryan Donnelly <ryanjdonnelly@gmail.com>
2026-02-11 16:21:56 -0500

    add TS26 and P26

--- src/data/Expansion.ts
+++ src/data/Expansion.ts
@@ -11,12 +11,14 @@ const VALUES = [
   'IBH', // 68, Intro Battle: Hoth
   'SEC', // 73, Secrets of Power
   'LAW', // 93, A Lawless Time
+  'TS26', // 103, 2026 Twin Suns
 
   'C24', // 13, 2024 Convention Exclusive
   'J24', // 28, 2024 Judge Program
   'J25', // 33, 2025 Judge Program
   'P25', // 38, 2025 Promo
   'C25', // 63, 2025 Convention Exclusive
+  'P26', // 83, 2026 Promo
 ] as const;
 export const ExpansionEnum = z.enum(VALUES);
 export type Expansion = z.infer<typeof ExpansionEnum>;
@@ -30,12 +32,14 @@ export const ExpansionRank: Record<Expansion, number> = {
   IBH: 6,
   SEC: 7,
   LAW: 8,
+  TS26: 9,
 
-  C24: 8,
-  J24: 9,
-  J25: 10,
-  P25: 11,
-  C25: 12,
+  C24: 10,
+  J24: 11,
+  J25: 12,
+  P25: 13,
+  C25: 14,
+  P26: 15,
 } as const;
 
 export const ExpansionCodes: Record<Expansion, number> = {
@@ -47,12 +51,14 @@ export const ExpansionCodes: Record<Expansion, number> = {
   IBH: 68,
   SEC: 73,
   LAW: 93,
+  TS26: 103,
 
   C24: 13,
   J24: 28,
   J25: 33,
   P25: 38,
   C25: 63,
+  P26: 83,
 } as const;
 
 export const ExpansionNames: Record<Expansion, string> = {
@@ -64,10 +70,12 @@ export const ExpansionNames: Record<Expansion, string> = {
   IBH: 'Intro Battle: Hoth',
   SEC: 'Secrets of Power',
   LAW: 'A Lawless Time',
+  TS26: '2026 Twin Suns',
 
   C24: '2024 Convention Exclusive',
   J24: '2024 Judge Program',
   J25: '2025 Judge Program',
   P25: '2025 Promo',
   C25: '2025 Convention Exclusive',
+  P26: '2026 Promo',
 } as const;
