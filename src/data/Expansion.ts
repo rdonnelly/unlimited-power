a78368f6127c1943090739644c93f5288d1c2c5e
Ryan Donnelly <ryanjdonnelly@gmail.com>
2026-05-15 20:18:58 -0400

    add ASH, remove judge expansions

--- src/data/Expansion.ts
+++ src/data/Expansion.ts
@@ -11,11 +11,10 @@ const VALUES = [
   'IBH', // 68, Intro Battle: Hoth
   'SEC', // 73, Secrets of Power
   'LAW', // 93, A Lawless Time
+  'ASH', // 108, Ashes of the Empire
   'TS26', // 103, 2026 Twin Suns
 
   'C24', // 13, 2024 Convention Exclusive
-  'J24', // 28, 2024 Judge Program
-  'J25', // 33, 2025 Judge Program
   'P25', // 38, 2025 Promo
   'C25', // 63, 2025 Convention Exclusive
   'P26', // 83, 2026 Promo
@@ -32,14 +31,13 @@ export const ExpansionRank: Record<Expansion, number> = {
   IBH: 6,
   SEC: 7,
   LAW: 8,
-  TS26: 9,
+  ASH: 9,
+  TS26: 10,
 
-  C24: 10,
-  J24: 11,
-  J25: 12,
-  P25: 13,
-  C25: 14,
-  P26: 15,
+  C24: 11,
+  P25: 12,
+  C25: 13,
+  P26: 14,
 } as const;
 
 export const ExpansionCodes: Record<Expansion, number> = {
@@ -51,11 +49,10 @@ export const ExpansionCodes: Record<Expansion, number> = {
   IBH: 68,
   SEC: 73,
   LAW: 93,
+  ASH: 108,
   TS26: 103,
 
   C24: 13,
-  J24: 28,
-  J25: 33,
   P25: 38,
   C25: 63,
   P26: 83,
@@ -70,11 +67,10 @@ export const ExpansionNames: Record<Expansion, string> = {
   IBH: 'Intro Battle: Hoth',
   SEC: 'Secrets of Power',
   LAW: 'A Lawless Time',
+  ASH: 'Ashes of the Empire',
   TS26: '2026 Twin Suns',
 
   C24: '2024 Convention Exclusive',
-  J24: '2024 Judge Program',
-  J25: '2025 Judge Program',
   P25: '2025 Promo',
   C25: '2025 Convention Exclusive',
   P26: '2026 Promo',
